a8b3b896391a8a1cb64eb574d9d59d6ccf527247
TKriel_ST10467193 <krieltristan68@gmail.com>
2026-02-23 21:33:46 +0200

    Added Events Controller.

---
+++ EventBOOKR/Controllers/EventsController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EventBOOKR.Data;
+using EventBOOKR.Models;
+
+namespace EventBOOKR.Controllers;
+
+public class EventsController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public EventsController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.Events.ToListAsync());
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("EventId,Name,Description,ImageUrl")] Event @event)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(@event);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(@event);
+    }
+}
