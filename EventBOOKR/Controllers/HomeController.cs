4fc0d6edc2f22d5e6fe0764e74b5b0fe4a45a2be
TKriel_ST10467193 <krieltristan68@gmail.com>
2026-03-02 17:35:42 +0200

    Updated homecontroller and dashboard html view to add custom homepage design.

--- EventBOOKR/Controllers/HomeController.cs
+++ EventBOOKR/Controllers/HomeController.cs
@@ -1,13 +1,27 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using EventBOOKR.Models;
+using EventBOOKR.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace EventBOOKR.Controllers;
 
 public class HomeController : Controller
 {
-    public IActionResult Index()
+    private readonly ApplicationDbContext _context;
+
+    public HomeController(ApplicationDbContext context)
     {
+        _context = context;
+    }
+    
+    public async Task<IActionResult> Index()
+    {
+        // Using viewbag to pass temporary data to the View
+        ViewBag.TotalVenues = await _context.Venues.CountAsync();
+        ViewBag.TotalEvents = await _context.Events.CountAsync();
+        ViewBag.TotalBookings = await _context.Bookings.CountAsync();
+        
         return View();
     }
 
